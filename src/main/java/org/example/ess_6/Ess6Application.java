4c23f4633c5e76e350a4985ccf50aca8a89d7fb9
TeoField-Marsham <126777160+TeoField-Marsham@users.noreply.github.com>
2024-01-05 14:02:41 +0100

    created github repo

---
+++ src/main/java/org/example/ess_6/Ess6Application.java
@@ -0,0 +1,13 @@
+package org.example.ess_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ess6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ess6Application.class, args);
+    }
+
+}
